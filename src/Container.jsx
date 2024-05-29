da9bdda287f46acfa05604d77640f90672ee6ac4
miawithcode <cuttontail@gmail.com>
2024-05-29 16:47:13 +0800

    feat: main content component

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,5 +1,13 @@
+import Stats from './Stats';
+import Textarea from './Textarea';
+
 const Container = () => {
-  return <main>Container</main>;
+  return (
+    <main>
+      <Textarea />
+      <Stats />
+    </main>
+  );
 };
 
 export default Container;
---
+++ src/Stats.jsx
@@ -0,0 +1,5 @@
+const Stats = () => {
+  return <section>Stats</section>;
+};
+
+export default Stats;
