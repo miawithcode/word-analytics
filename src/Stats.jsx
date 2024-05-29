6869701c5faf674fe84b8ef6bddcaa2df2a84ce5
miawithcode <cuttontail@gmail.com>
2024-05-30 06:24:57 +0800

    feat: Stats component

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,5 +1,21 @@
 const Stats = () => {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat number={0} label="Words" />
+      <Stat number={0} label="Characters" />
+      <Stat number={280} label="Twitter" />
+      <Stat number={2200} label="Facebook" />
+    </section>
+  );
+};
+
+const Stat = ({ number, label }) => {
+  return (
+    <section className="stat">
+      <span className="stat-number">{number}</span>
+      <h2 className="stat-label">{label}</h2>
+    </section>
+  );
 };
 
 export default Stats;
